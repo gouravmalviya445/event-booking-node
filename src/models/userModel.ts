3564b120590fa0bdff0d542a027152f52f65942e
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-12-22 20:30:16 +0530

    add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { ENV } from "../env";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  role: "user" | "admin";
+
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "organizer"],
+      default: "user"
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+})
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.createAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      role: this.role,
+      email: this.email
+    },
+    ENV.jwtSecret,
+    { expiresIn: ENV.jwtExpiry } as jwt.SignOptions
+  )
+}
+
+const User = mongoose.model<IUser>("User", userSchema);
+export { User };
