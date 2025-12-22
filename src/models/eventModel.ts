0ccfb6492b45d9b7c1181ece77446dbfbca1c39f
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-12-22 21:24:34 +0530

    add event model

---
+++ src/models/eventModel.ts
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IEvent {
+  title: string;
+  description: string;
+  location: string;
+  date: Date;
+  price: number;
+  totalSeats: number;
+  availableSeats: number;
+  status: "active" | "cancelled";
+  // startTime: string;
+  // endTime: string;
+}
+
+const eventSchema: Schema<IEvent> = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+
+    // TODO:
+    // startTime: String,
+    // endTime: String,
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    totalSeats: {
+      type: Number,
+      required: true
+    },
+    availableSeats: {
+      type: Number,
+      required: true
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "cancelled"],
+      default: "active"
+    }
+  },
+  { timestamps: true }
+);
+
+export const Event = mongoose.model<IEvent>("Event", eventSchema);
