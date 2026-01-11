7e5eb0547c808ab5ae94a380e18749542d3de553
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-12 02:25:57 +0530

    add validation for reset pass verify otp

--- src/schema/authValidation.ts
+++ src/schema/authValidation.ts
@@ -2,6 +2,12 @@ import z from "zod";
 
 const emailBodySchema = z.object({ email: z.email() });
 
+const verifyPassSchema = z.object({
+  email: z.email(),
+  otp: z.string().length(6, "otp must be 6 digit")
+})
+
 export {
-  emailBodySchema
+  emailBodySchema,
+  verifyPassSchema
 }
