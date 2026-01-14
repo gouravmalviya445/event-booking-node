7ce0631fcb7546c88a095bf0ebc95983e6a329e4
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-14 23:28:52 +0530

    add otpRateLimiter for email and password OTP sending

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { sendEmailOtp, verifyEmailOtp, sendResetPasswordOtp, verifyResetPasswordOtp, resetPassword } from "../controllers/authController";
 import { userAuth } from "../middlewares/authMiddleware"
+import { otpRateLimiter } from "../middlewares/rateLimitMiddleware";
 
 const router = Router();
 
 // email verification
-router.post("/email/send", userAuth, sendEmailOtp);
+router.post("/email/send", userAuth, otpRateLimiter, sendEmailOtp);
 router.post("/email/verify", userAuth, verifyEmailOtp);
 
 // reset password
-router.post("/password/send", sendResetPasswordOtp);
+router.post("/password/send", otpRateLimiter, sendResetPasswordOtp);
 router.post("/password/verify", verifyResetPasswordOtp)
 router.post("/password/reset", resetPassword)
 
