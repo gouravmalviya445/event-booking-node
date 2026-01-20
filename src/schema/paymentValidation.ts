19411fd51e36a2ad186504de943d8cddd780af1e
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-21 00:07:56 +0530

    add payment body input validation

---
+++ src/schema/paymentValidation.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+const createPaymentOrderSchema = z.object({
+  eventId: z.string(),
+  amount: z.number(),
+  currency: z.string().default("INR"),
+})
+
+const verifyPaymentSchema = z.object({
+  razorpay_payment_id: z.string(),
+  razorpay_order_id: z.string(),
+  razorpay_signature: z.string(),
+})
+
+export {
+  createPaymentOrderSchema,
+  verifyPaymentSchema
+}
