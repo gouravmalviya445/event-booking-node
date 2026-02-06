eace7c0218c84202247540510e0cf6e95e1606ec
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-02-06 23:50:28 +0530

    add getAllBookings controller

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -2,15 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import { ApiError, StatusCodes } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
-import z from "zod";
-import { bookingCreateSchema } from "../schema/bookingValidation";
-import { ENV } from "../env";
-import mongoose from "mongoose";
 import { apiClient } from "../utils/apiClient";
 
 const checkBookingStatus = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const orderId = String(req.query.orderId);
+    const orderId = req.params.OrderId;
 
     if (!orderId.trim()) {
       throw new ApiError(StatusCodes.BAD_REQUEST, "Order id is required", [], "");
@@ -41,6 +37,32 @@ const checkBookingStatus = asyncHandler(
   }
 )
 
+const getAllBookings = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { data: { data } } = await apiClient.get(`/api/bookings`);
+
+      res
+        .status(StatusCodes.OK)
+        .json(
+          new ApiResponse(
+            StatusCodes.OK,
+            "Bookings fetched successfully",
+            data
+          )
+        )
+    } catch (error: any) {
+      throw new ApiError(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        error?.response?.data?.error || "Error fetching bookings",
+        [],
+        ""
+      );
+    }
+  }
+)
+
 export {
-  checkBookingStatus
+  checkBookingStatus,
+  getAllBookings
 }
